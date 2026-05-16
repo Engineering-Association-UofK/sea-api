9025aa35a308280890820f78659ff1c2f1a085ab
Abdulrahim <unvacc80@gmail.com>
2026-05-16 03:22:11 +0300

    Fix misnaming

--- internal/services/event/application_manager.go
+++ internal/services/event/application_manager.go
@@ -6,7 +6,7 @@ import (
 	"sea-api/internal/utils/valid"
 )
 
-func (s *EventService) GetEventParticipants(userID int64, req models.ListRequest) ([]models.EventParticipantsResponse, error) {
+func (s *EventService) GetEventParticipants(eventID int64, req models.ListRequest) ([]models.EventParticipantsResponse, error) {
 	total, err := s.EventRepo.GetTotalEvents()
 	if err != nil {
 		return nil, err
@@ -14,14 +14,14 @@ func (s *EventService) GetEventParticipants(userID int64, req models.ListRequest
 
 	valid.Limit(&req, total)
 
-	participants, err := s.EventRepo.GetEventParticipantsList(req, userID)
+	participants, err := s.EventRepo.GetEventParticipantsList(req, eventID)
 	if err != nil {
 		return nil, err
 	}
 
 	return []models.EventParticipantsResponse{
 		{
-			EventID:      userID,
+			EventID:      eventID,
 			Participants: participants,
 			Page:         req.Page,
 			Current:      req.Page,
