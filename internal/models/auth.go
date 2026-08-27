6fcddccfe1ceb0b2ce0e4899a03769178c50e42e
Abdulrahim <unvacc80@gmail.com>
2026-08-27 19:19:24 +0200

    Update auth models and repositories
    - Add models for registration steps
    - Update repositories with needed functions

--- internal/models/auth.go
+++ internal/models/auth.go
@@ -1,14 +1,34 @@
 package models
 
 import (
+	"encoding/json"
 	"time"
 
+	_ "github.com/go-playground/validator/v10"
 	"github.com/golang-jwt/jwt/v5"
 )
 
 type RegistrationStepModel struct {
-	UserID int64 `db:"user_id"`
-	Step   int64 `db:"step"`
+	RegCode string `db:"reg_code"`
+	UserID  int64  `db:"user_id"`
+	Step    int64  `db:"step"`
+}
+
+type RegInitCreate struct {
+	ID    int64         `db:"id"`
+	Email TrimmedString `db:"email"`
+}
+
+type RegDetailsUpdate struct {
+	UserID int64  `db:"user_id"`
+	NameAr string `db:"name_ar"`
+	NameEn string `db:"name_en"`
+	Gender Gender `db:"gender"`
+
+	UniID      string     `db:"uni_id"`
+	Department Department `db:"department"`
+
+	Phone string `db:"phone"`
 }
 
 type LoginRequest struct {
@@ -17,10 +37,18 @@ type LoginRequest struct {
 }
 
 type LoginResponse struct {
-	Token      string `json:"token"`
-	UserID     int64  `json:"user_id"`
-	Roles      []Role `json:"roles"`
-	IsVerified bool   `json:"is_verified"`
+	Token       string `json:"token"`
+	UserID      int64  `json:"user_id"`
+	Roles       []Role `json:"roles"`
+	IsVerified  bool   `json:"is_verified"`
+	RedirectURL string `json:"redirect_url"`
+}
+
+type ForgotPasswordRequest struct {
+	UserID   *int64   `json:"user_id"`
+	Email    *string  `json:"email"`
+	Lang     Language `json:"lang" binding:"required"`
+	Username *string  `json:"username"`
 }
 
 type ManagedClaims struct {
@@ -32,23 +60,52 @@ type ManagedClaims struct {
 	jwt.RegisteredClaims
 }
 
-type RegisterRequest struct {
-	UserID   int64         `json:"user_id" binding:"required"`
-	UniID    string        `json:"uni_id" binding:"required"`
-	Username TrimmedString `json:"username" binding:"required,min=3,max=20"`
-	Email    TrimmedString `json:"email" binding:"required,email"`
+// Registration structs
 
-	NameAr string `json:"name_ar" binding:"required"`
-	NameEn string `json:"name_en" binding:"required"`
-	Phone  string `json:"phone"`
-
-	Passcode        string `json:"passcode" binding:"required"`
-	Password        string `json:"password" binding:"required"`
-	ConfirmPassword string `json:"confirm_password" binding:"required"`
-
-	Department Department `json:"department" binding:"required"`
-	Gender     Gender     `json:"gender" binding:"required"`
+type RegistrationRequest struct {
+	Step int64           `json:"step"`
+	Data json.RawMessage `json:"data"`
 }
+
+type InitialRegistrationRequest struct {
+	UserID   int64         `json:"user_id" validate:"required"`
+	Passcode string        `json:"passcode" validate:"required"`
+	Email    TrimmedString `json:"email" validate:"required,email"`
+	Lang     Language      `json:"lang" validate:"required"`
+}
+
+type PasswordRegistrationRequest struct {
+	RegCode         string `json:"reg_code" validate:"required"`
+	Password        string `json:"password" validate:"required"`
+	ConfirmPassword string `json:"confirm_password" validate:"required"`
+}
+
+type DetailsRegistrationRequest struct {
+	RegCode string `json:"reg_code" validate:"required"`
+	NameAr  string `json:"name_ar" validate:"required"`
+	NameEn  string `json:"name_en" validate:"required"`
+	Gender  Gender `json:"gender" validate:"required"`
+
+	UniID      string     `json:"uni_id" validate:"required"`
+	Department Department `json:"department" validate:"required"`
+
+	Phone string `json:"phone"`
+}
+
+type UsernameRegistrationRequest struct {
+	RegCode  string        `json:"reg_code" validate:"required"`
+	Username TrimmedString `json:"username" validate:"required,min=3,max=20"`
+}
+
+type CheckRegistrationRequest struct {
+	RegCode string `json:"reg_code" binding:"required"`
+}
+
+type CheckRegistrationResponse struct {
+	RegStep int64 `json:"reg_step"`
+}
+
+// End
 
 type VerificationCodeModel struct {
 	ID        int64     `db:"id"`
--- internal/repositories/auth.go
+++ internal/repositories/auth.go
@@ -15,21 +15,43 @@ func NewAuthRepository(db *sqlx.DB) *AuthRepository {
 	return &AuthRepository{db: db}
 }
 
-func (r *AuthRepository) StartRegistration(userID int64) error {
-	query := fmt.Sprintf(`INSERT INTO %s (user_id, step) VALUES (?, ?)`, models.TableRegistrationStep)
-	_, err := r.db.Exec(query, userID, 1)
+func (r *AuthRepository) StartRegistration(model *models.RegistrationStepModel) error {
+	model.Step = 1
+	query := fmt.Sprintf(`INSERT INTO %s (reg_code, user_id, step) 
+		VALUES (:reg_code, :user_id, :step)`, models.TableRegistrationStep)
+
+	_, err := r.db.NamedExec(query, model)
 	return err
 }
 
-func (r *AuthRepository) IncrementStep(userID int64) error {
-	query := fmt.Sprintf(`UPDATE %s SET step = step + 1 WHERE user_id = ?`, models.TableRegistrationStep)
-	_, err := r.db.Exec(query, userID)
+func (r *AuthRepository) IncrementStep(RegCode string) error {
+	query := fmt.Sprintf(`UPDATE %s SET step = step + 1 WHERE reg_code = ?`, models.TableRegistrationStep)
+	_, err := r.db.Exec(query, RegCode)
 	return err
 }
 
-func (r *AuthRepository) GetState(userID int64) (*models.RegistrationStepModel, error) {
-	var model *models.RegistrationStepModel
+func (r *AuthRepository) DecrementStep(RegCode string) error {
+	query := fmt.Sprintf(`UPDATE %s SET step = step - 1 WHERE reg_code = ?`, models.TableRegistrationStep)
+	_, err := r.db.Exec(query, RegCode)
+	return err
+}
+
+func (r *AuthRepository) SetStep(RegCode string, stepNumber int64) error {
+	query := fmt.Sprintf(`UPDATE %s SET step = step - ? WHERE reg_code = ?`, models.TableRegistrationStep)
+	_, err := r.db.Exec(query, stepNumber, RegCode)
+	return err
+}
+
+func (r *AuthRepository) GetStateWithCode(RegCode string) (*models.RegistrationStepModel, error) {
+	var model models.RegistrationStepModel
+	query := fmt.Sprintf(`SELECT * FROM %s WHERE reg_code = ?`, models.TableRegistrationStep)
+	err := r.db.Get(&model, query, RegCode)
+	return &model, err
+}
+
+func (r *AuthRepository) GetStateWithID(ID int64) (*models.RegistrationStepModel, error) {
+	var model models.RegistrationStepModel
 	query := fmt.Sprintf(`SELECT * FROM %s WHERE user_id = ?`, models.TableRegistrationStep)
-	err := r.db.Get(model, query, userID)
-	return model, err
+	err := r.db.Get(&model, query, ID)
+	return &model, err
 }
