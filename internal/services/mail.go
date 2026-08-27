4775127d3ce8814184838d32318cacdbc5d3f147
Abdulrahim <unvacc80@gmail.com>
2026-08-27 19:21:03 +0200

    Use new email templates

--- internal/services/mail.go
+++ internal/services/mail.go
@@ -42,6 +42,36 @@ func (m *MailService) SendVerificationCode(to string, data models.VerifyEmail) e
 	})
 }
 
+func (m *MailService) SendRegistrationMail(to, link string, lang models.Language) error {
+	tem, err := utils.GetEmailTemplate(models.EmailAccLink, lang, struct {
+		Link string
+		Year int
+	}{Link: link, Year: time.Now().Year()})
+	if err != nil {
+		return err
+	}
+	return m.SendEmail(models.Email{
+		To:      []string{to},
+		Subject: "Registration process started",
+		HTML:    tem,
+	})
+}
+
+func (m *MailService) SendForgotPassMail(to, link string, lang models.Language) error {
+	tem, err := utils.GetEmailTemplate(models.EmailAccPassReset, lang, struct {
+		Link string
+		Year int
+	}{Link: link, Year: time.Now().Year()})
+	if err != nil {
+		return err
+	}
+	return m.SendEmail(models.Email{
+		To:      []string{to},
+		Subject: "Password Reset Request",
+		HTML:    tem,
+	})
+}
+
 func (m *MailService) SendEmail(e models.Email) error {
 
 	recipients := append([]string{}, e.To...)
